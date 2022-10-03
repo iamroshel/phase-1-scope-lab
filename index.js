49cde80f9863eaddc2927470d144fc4235139c78
Roshel Esteron <mynameisroshel19@gmail.com>
2022-10-03 12:37:42 +1000

    commit tree 2f76e2af666de0d3093ac34ad7ac65a77cd7ab91

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = "Eden"; 
+ 
+function upperCaseCustomerName() {
+    
+    return customerName = customerName.toUpperCase();
+}; 
+ 
+function setBestCustomer() {
+
+    bestCustomer = 'not bob'
+
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Vanessa";
+    return leastFavoriteCustomer;
+}
